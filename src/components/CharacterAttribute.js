ece49f3bfedd5a9f12f0b01221366900b7e44ea2
Dušan Juretić <dusan.juretic@gmail.com>
2016-09-16 12:40:27 -0300

    Can edit hp and mp

--- src/components/CharacterAttribute.js
+++ src/components/CharacterAttribute.js
@@ -5,17 +5,29 @@ import GameNumberInput from './GameNumberInput';
 var CharacterAttribute = React.createClass({
 	propTypes: {
 		name: React.PropTypes.string.isRequired,
-		var: React.PropTypes.string.isRequired,
+		var: React.PropTypes.string,
+		vars: React.PropTypes.arrayOf(React.PropTypes.string),
 		value: React.PropTypes.number,
+		values: React.PropTypes.arrayOf(React.PropTypes.number),
 		onChange: React.PropTypes.func.isRequired,
 	},
 	render() {
+		var inputs;
+		if(this.props.vars) {
+			inputs = (
+				<Row>
+					<Col md={5}><GameNumberInput var={this.props.vars[0]} value={this.props.values[0]} onChange={this.props.onChange}/></Col>
+					<Col md={1}>/</Col>
+					<Col md={5}><GameNumberInput var={this.props.vars[1]} value={this.props.values[1]} onChange={this.props.onChange}/></Col>
+				</Row>
+			);
+		} else {
+			inputs = <GameNumberInput var={this.props.var} value={this.props.value} onChange={this.props.onChange}/>;
+		}
 		return (
 			<Row>
 				<Col md={6} className="text-right">{this.props.name}</Col>
-				<Col md={6}>
-					<GameNumberInput var={this.props.var} value={this.props.value} onChange={this.props.onChange}/>
-				</Col>
+				<Col md={6}>{inputs}</Col>
 			</Row>
 		);
 	}
--- src/components/CharacterProfile.js
+++ src/components/CharacterProfile.js
@@ -8,18 +8,9 @@ var CharacterProfile = React.createClass({
 			<fieldset>
 				<legend>Character</legend>
 				<img src={this.props.gender === 1 ? require('../../assets/hero_female.png'): require('../../assets/hero_male.png')} className="center-block"/>
-				<Row>
-					<Col md={6} className="text-right">HP</Col>
-					<Col md={6}>{this.props.hp} / {this.props.maxHp}</Col>
-				</Row>
-				<Row>
-					<Col md={6} className="text-right">Mana</Col>
-					<Col md={6}>{this.props.mp} / {this.props.maxMp}</Col>
-				</Row>
-				<Row>
-					<Col md={6} className="text-right">Armor Class</Col>
-					<Col md={6}>{this.props.armorClass}</Col>
-				</Row>
+				<CharacterAttribute name="HP" vars={['hp', 'maxHp']} values={[this.props.hp, this.props.maxHp]} onChange={this.props.handleChange}/>
+				<CharacterAttribute name="Mana" vars={['mp', 'maxMp']} values={[this.props.mp, this.props.maxMp]} onChange={this.props.handleChange}/>
+				<CharacterAttribute name="Armor Class" var="armorClass" value={this.props.armorClass} onChange={this.props.handleChange}/>
 				<CharacterAttribute name="Experience" var="exp" value={this.props.exp} onChange={this.props.handleChange}/>
 				<CharacterAttribute name="Strength" var="str" value={this.props.str} onChange={this.props.handleChange}/>
 				<CharacterAttribute name="Intelligence" var="int" value={this.props.int} onChange={this.props.handleChange}/>
